d50e363bfc1eede31b17a1aeeef1f97897bf6814
Konstantin Motuskov <konstantin.motuskov@wilo.com>
2022-10-29 16:51:51 +0300

    Add models to administration interface

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(EmployeePosition)
+class EmployeePositionAdmin(admin.ModelAdmin):
+    '''
+    The EmployeePositionAdmin object represents a position of an
+    employee in the administration interface.
+    '''
+    list_display = (
+        'title',
+    )
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    '''
+    The DepartmentAdmin object represents a department of a compony
+    in administration interface.
+    '''
+    list_display = (
+        'name',
+        'parent_department',
+    )
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    '''
+    The EmployeeAdmin represents en employee of a company in
+    administration interface.
+    '''
+    list_display = (
+        'last_name',
+        'first_name',
+        'middle_name',
+        'department',
+        'position',
+    )
