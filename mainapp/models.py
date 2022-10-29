2e85331ccb60fb911b7534575518ca2733078ec5
Konstantin Motuskov <konstantin.motuskov@wilo.com>
2022-10-29 16:46:44 +0300

    Realize __str__ functions for the model classes

--- mainapp/models.py
+++ mainapp/models.py
@@ -13,6 +13,9 @@ class EmployeePosition(models.Model):
         unique=True
     )
 
+    def __str__(self):
+        return self.title
+
 class Department(models.Model):
     '''
     The Department object represents a department of a compony.
@@ -31,6 +34,9 @@ class Department(models.Model):
         null=True,
         related_name='child_departments'
     )
+
+    def __str__(self):
+        return self.name
 
 class Employee(models.Model):
     '''
@@ -79,3 +85,9 @@ class Employee(models.Model):
                 'middle_name'
             ])
         ]
+
+    def __str__(self):
+        if self.middle_name:
+            return f'{self.last_name} {self.first_name} {self.middle_name}'
+        else:
+            return f'{self.last_name} {self.first_name}'
